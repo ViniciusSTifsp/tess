dd2281976590011ed5729ab09792b4603ac8925f
ViniciusSTifsp <141365154+ViniciusSTifsp@users.noreply.github.com>
2024-11-12 11:07:04 -0300

    Update Connection.php

--- config/Connection.php
+++ config/Connection.php
@@ -5,7 +5,7 @@ class Connection {
     private $host = 'localhost';
     private $username = '3306';
     private $password = '';
-    private $database = 'tess_oficial';
+    private $database = 'hostdeprojetos_tess';
 
     public function connection() {
         $con = new mysqli($this->host, $this->username, $this->password, $this->database);
